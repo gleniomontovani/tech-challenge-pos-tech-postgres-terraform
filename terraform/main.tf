f7b9699e6e9a114475d3f78f355e0a8cd50194ea
Glenio Montovani <gleniomontovani@gmail.com>
2024-03-17 17:58:25 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
+  backend "s3" {
+    bucket = terraform-tfstates
+    key    = "sonarqube/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
+  profile = "default" 
   region = "us-east-1"
 }
 
