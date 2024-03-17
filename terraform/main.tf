c61f7cff6e19185d111672aaca2524a6da833f33
Glenio Montovani <gleniomontovani@gmail.com>
2024-03-17 18:27:39 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,8 @@ terraform {
 provider "aws" {
   profile = "default" 
   region = "us-east-1"
+  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 }
 
 resource "aws_db_instance" "postgres" {
