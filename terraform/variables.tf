572808f5b8568734d1805981a299293e9a5d0122
Glenio Montovani <gleniomontovani@gmail.com>
2024-03-17 18:32:30 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,11 @@ variable "postgres_username" {
 variable "postgres_password" {
   description = "Password for PostgreSQL"
 }
+
+variable "aws_access_key" {
+  description = "ACCESS KEY AWS"
+}
+
+variable "aws_secret_key" {
+  description = "SECRET ACCESS AWS"
+}
