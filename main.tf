e7e69114b8b1cd7a2330110b9893d93210fd4f1f
Glenio Montovani <gleniomontovani@gmail.com>
2024-03-17 16:48:01 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "postgres" {
+  identifier           = "my-postgres-db"
+  engine               = "postgres"
+  instance_class       = "db.t2.micro"
+  username             = var.postgres_username
+  password             = var.postgres_password
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  publicly_accessible = false
+
+  tags = {
+    Name = "MyPostgresDB"
+  }
+}
+
+output "postgres_db_endpoint" {
+  value = aws_db_instance.postgres.endpoint
+}
