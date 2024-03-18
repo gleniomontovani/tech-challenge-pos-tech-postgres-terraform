1f392050395f4e0507a89dbd1fbc06000f719530
Glenio Montovani <gleniomontovani@gmail.com>
2024-03-18 16:58:31 -0300

    Update userdata.sh

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,6 +1,3 @@
 sudo echo "<?php
-define('DB_SERVER', '${rds_endpoint}');
-define('DB_USERNAME', 'mysqldb');
-define('DB_PASSWORD', 'mysql123a');
-define('DB_DATABASE', 'mysqldb');
+define('POSTGRES_ENDPOINT', '${rds_endpoint}');
 ?>" > dbinfo.inc
