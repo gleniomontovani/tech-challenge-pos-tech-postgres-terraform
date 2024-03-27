bbaea4cf909f224ee72263fea55a69e296102fbb
gleniomontovani <gleniomontovani@gmail.com>
2024-03-27 16:03:52 -0300

    Ajustando configuração da subnets

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,4 +1,4 @@
 resource "aws_db_subnet_group" "subnet-rds" {
   name       = "subnet-rds-${var.projectName}"
-  subnet_ids = [var.subnet_03_id, var.subnet_02_id, var.subnet_03_id]
+  subnet_ids = [var.subnet_01_id, var.subnet_02_id, var.subnet_03_id]
 }
